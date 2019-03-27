471c2dba49e4a8e7283e9977386fff3fc65d53b9
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-03-27 21:37:14 +0300

    Add files via upload

---
+++ 16.lisp
@@ -0,0 +1,31 @@
+(defun APPEND (X Y) 
+
+		(cond 
+			((null X) Y) 
+				(T (cons (car X) (APPEND (cdr X) Y))))) 
+
+	(defun appendp ( 
+
+	(defun insertp (list1 list2 i) 
+
+		(cond 
+			((null list1) list2) 
+
+			((null list2) list1) 
+				((equal i 0) 
+				
+				(append list2 list1)) 
+
+				(t (cons (car list1) 
+
+					(insertp (cdr list1) list2 
+
+					(- i 1)) 
+
+) 
+) 
+) 
+) 
+(print(insertp '(a b c d f g) '(0 0 0) 2)) ;(A B 0 0 0 C D F G) 
+
+(print(insertp '(1 3 5) '(2 4 6) 0)); (2 4 6 1 3 5)
