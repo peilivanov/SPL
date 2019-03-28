96a7a7094b166fcddd1242150a8e5af4470356cb
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-03-28 08:16:23 +0200

    Update 16.lisp

--- 16.lisp
+++ 16.lisp
@@ -1,5 +1,13 @@
 ; 16. Определить функцию, добавляющую элементы одного списка во второй список, начиная с заданной позиции.;
 
+(defun append (x y)
+	(cond
+		(
+		  (null x) y)
+		(T(cons car x
+			appendp(cdr x)y)
+		  ))))
+
 (defun insertp (list1 list2 i) 
 
 	(cond 
@@ -7,7 +15,7 @@
 			((null list2) list1) 
 				((equal i 0) 
 				
-				(append list2 list1)) 
+				(appendp list2 list1)) 
 
 				(t (cons (car list1) 
 
@@ -15,10 +23,8 @@
 
 					(- i 1)) 
 
-) 
-) 
-) 
-) 
+) ) ) ) 
+
 (print(insertp '(a b c d f g) '(0 0 0) 2)) ;(A B 0 0 0 C D F G) 
 
 (print(insertp '(1 3 5) '(2 4 6) 0)); (2 4 6 1 3 5)
