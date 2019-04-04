a0d8966b30c2728967fc2bb923b2bf9063e9846a
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-04-04 07:10:32 +0200

    Update 21.lisp

--- 21.lisp
+++ 21.lisp
@@ -1,10 +1,18 @@
 ;21. Определите функцию, удаляющую из списка первое вхождение данного элемента на верхнем уровне.
 
-(defun del (L X)
- (cond
-  ((null L) nil)
-  ((equal (car L) X) (cdr L))
-  (T (cons (car L) (del (cdr L) X)))))
+(defun Del (mainList forDelete)
+   ((lambda (head tail) 
+      (cond 
+         ((null mainList) nil)
+         ((equal head forDelete) tail)
+         (t (cons head (Del tail forDelete)))
+      )
+    )
+    (car mainList)
+    (cdr mainList)
+   )
+)
 
-(print (del '(7 7 (8 8)) 7)); (7 (8 8))
-(print (del '(7 7 (8 8)) 8)); (7 7 (8 8)) 
+(print( Del '((1 2 ) 4 5 (1 2 3) 6)  '(1 2 3))) ; ==> ((1 2) 4 5 6) 
+
+ 
