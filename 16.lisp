1b104cfab96f6c0c76e3a8eb320d40063c5df77f
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-04-17 18:47:33 +0300

    Update 16.lisp

--- 16.lisp
+++ 16.lisp
@@ -3,25 +3,25 @@
 
 (defun appendp (x y)
 	(cond
-		(
-		  (null x) y)
-		(T(cons (car x)
-			(appendp(cdr x)y)
-		  ))))
+		((null x) y)
+			(T(cons (car x)
+			(appendp(cdr x)y))
+		)
+	)
+)
 
 (defun insertp (src dest pos) 
     (cond 
-		((null src) 
-         	dest) 
-			((null dest) src) 
-				((equal pos 0) 
-				(appendp dest src)) 
-                (t (cons (car src) 
-                     (insertp (cdr src)
-                               dest 
-                                (- pos 1)) 
-
-) ) ) ) 
+	((null src)dest) 
+		((null dest) src) 
+		((equal pos 0)(appendp dest src)) 
+                	(t (cons (car src) 
+                     	(insertp (cdr src)dest 
+                                (- pos 1))
+			)
+		)
+     )
+)
 
 (print(insertp '(a b c d f g) '(0 0 0) 2)) ;(A B 0 0 0 C D F G) 
 
