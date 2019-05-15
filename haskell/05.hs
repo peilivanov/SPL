6e234f306e514f6afdea55b6b36b7bdf03a8be77
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-05-15 21:59:14 +0300

    Update 05.hs

--- haskell/05.hs
+++ haskell/05.hs
@@ -3,4 +3,4 @@ connector (x:[]) = [x]
 connector (x:xs) | x == head xs = x : connector (drop 1 xs)
                | otherwise = x : connector xs
                
--- *Main> remSame [2, 2, 1, 1]
+-- *Main> connector [2, 2, 1, 1]
