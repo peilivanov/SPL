3e3705a0fd25035652b266fe48fd8ba3d726017c
peilivanov <45008165+peilivanov@users.noreply.github.com>
2019-05-16 07:20:52 +0200

    Update 05.hs

--- haskell/05.hs
+++ haskell/05.hs
@@ -2,5 +2,5 @@ connector [] = []
 connector (x:[]) = [x]
 connector (x:xs) | x == head xs = x : connector (drop 1 xs)
                | otherwise = x : connector xs
-               
--- *Main> connector [2, 2, 1, 1]
+         
+main = print $ connector [2, 2, 1, 1]
